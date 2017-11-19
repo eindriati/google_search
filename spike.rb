08a5c0664141303fed3890b973e617fcd01e7d6a
EI <Rayhana@Rayhanas-MacBook-Pro.local>
2017-11-19 15:55:41 -0500

    print the first result

--- spike.rb
+++ spike.rb
@@ -1,10 +1,25 @@
 require 'watir'
+require 'byebug'
 
 browser = Watir::Browser.new
 
-browser.goto 'watir.com'
-browser.link(text: 'documentation').click
+browser.goto 'google.com'
 
-puts browser.title
-# => 'Documentation – Watir Project...'
-browser.close
+
+
+browser.text_field(name: "q").set "ruby code"
+# browser.button(name: "btnK").click
+
+browser.text_field(name: "q").send_keys :enter
+
+first_div = browser.divs(class: "g")[0]
+first_link = first_div.links[1].href
+
+
+puts "First result URL: #{first_link}"
+
+
+# byebug
+
+puts "end"
+#end
